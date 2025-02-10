01de8ddc06af20b11a6087bd05151755cbfaeba7
bibonut <laurelchow04@gmail.com>
2025-02-10 23:08:25 +0800

    improve UI

--- micro.py
+++ micro.py
@@ -1,6 +1,7 @@
 import collections
+import os
 """
-Command line script to check sign ins and outs for weekly sessions
+Command line program to check sign ins and outs for weekly sessions
 """
 sign_in_dict = {}
 sign_outs_dict = {}
@@ -13,6 +14,12 @@ duplicate_sign_outs = []
 
 
 def calc(sign_ins, sign_outs):
+    print('=============================================================================================\n')
+    print("Inputted sign in values:")
+    print(f"		{sign_ins}")
+    print("Inputted sign out values:")
+    print(f"		{sign_outs}\n")
+    
     sign_ins = sign_ins.split(',')
     sign_outs = sign_outs.split(',')
 
@@ -26,23 +33,29 @@ def calc(sign_ins, sign_outs):
     for number in sign_outs:
         if number not in sign_ins:
             no_sign_in.append()
-    
-    print("\n")
-    print(f"Number of sign ins inputted: {len(sign_ins)}")
-    print(f"Number of sign outs inputted: {len(sign_outs)}\n")
 
-    print(f"duplicate sign ins: {duplicate_sign_ins}")
-    print(f"duplicate sign outs: {duplicate_sign_outs}\n")
+    print('=============================================================================================\n')
+    print('Results:\n')
+    print(f'Number of sign ins inputted: {len(sign_ins)}')
+    print(f'Number of sign outs inputted: {len(sign_outs)}\n')
 
-    print(f"didnt sign in: {no_sign_in}")
-    print(f"didnt sign out: {no_sign_out}\n")
+    print(f'Duplicate sign ins: {duplicate_sign_ins}')
+    print(f'Duplicate sign outs: {duplicate_sign_outs}\n')
+
+    print(f'Didnt sign in: {no_sign_in}')
+    print(f'Didnt sign out: {no_sign_out}\n')
+
+    print('Please remember to double check results with QR hours and pay attention to the time of sign in/out!\n')
+    print('=============================================================================================\n')
 
 
 def main():
-    print('\n\nInput format:\nstudentnumber1,studentnumber2,studentnumber3 ...\n\n')
-    print('Example input:\n24069988,24002225,23952512,22974583,24132194,23902644,24254296\n')
-    sign_ins = input('\n\nPlease input the sign ins following the format\n')
-    sign_outs = input('\n\nPlease input the sign outs following the format\n')
+    print('\n=============================================================================================')
+    print('Input format:\nstudentnumber1,studentnumber2,studentnumber3...\n\n')
+    print('Example input:\n24069988,24002225,23952512,22974583,24132194,23902644,24254296...\n')
+    sign_ins = input('\n> Please input the sign ins in the required format:\n')
+    sign_outs = input('\n> Please input the sign outs in the required format:\n')
+    os.system('cls||clear')
     calc(sign_ins, sign_outs)
 
 if __name__ == "__main__":
